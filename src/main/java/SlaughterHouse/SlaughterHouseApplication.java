c2be80e4b93528391dd09bbcf659cb3d1ba00ab8
rajipkanxo01 <82520479+rajipkanxo01@users.noreply.github.com>
2023-03-23 21:04:24 +0100

    initial commit

---
+++ src/main/java/SlaughterHouse/SlaughterHouseApplication.java
@@ -0,0 +1,13 @@
+package SlaughterHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaughterHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlaughterHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/SlaughterHouse/SlaughterHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package SlaughterHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaughterHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
